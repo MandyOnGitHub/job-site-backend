25497dec7ceb0f91f9065c2ee07cd122ec91a461
MandyOnGitHub <Mandy.mailen@yahoo.com>
2022-12-18 23:45:17 +0100

    read job.json and send via /jobs route to frontend

--- dist/server.js
+++ dist/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import fs from 'fs';
 const app = express();
+// app.use(cors());
 // without JSON.parse buffer will be read
 const jobs = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf-8'));
 const port = 5000;
 app.get('/', (req, res) => {
     res.send('job site api');
+});
+app.get('/jobs', (req, res) => {
+    res.json(jobs);
 });
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import fs from 'fs'
-
-const app = express()
+import cors from 'cors';
+ 
+const app = express();
+app.use(cors());
 
 // without JSON.parse buffer will be read
 const jobs = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf-8'))
@@ -11,6 +13,9 @@ app.get('/', (req: express.Request, res: express.Response) => {
     res.send('job site api');
 })
 
+app.get('/jobs', (req: express.Request, res: express.Response) => {
+    res.json(jobs)
+})
 
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
